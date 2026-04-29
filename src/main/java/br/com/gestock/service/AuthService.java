fa2b7622dbf6193e6a3eb7fa7994cfd00f559c8b
Tiago-abr <ti.abreu.da.rocha@gmail.com>
2026-04-29 16:11:34 -0300

    feat: move login validation to AuthService and add validateLogin method

--- src/main/java/br/com/gestock/service/AuthService.java
+++ src/main/java/br/com/gestock/service/AuthService.java
@@ -1,5 +1,26 @@
 package br.com.gestock.service;
 
+import br.com.gestock.model.Usuario;
+import br.com.gestock.repository.UsuarioRepository;
+import br.com.gestock.service.exceptions.InvalidLoginException;
+import br.com.gestock.util.Criptografia;
+
 public class AuthService {
+    private UsuarioRepository UsuarioRepository;
     
+    public AuthService(UsuarioRepository UsuarioRepository){
+        this.UsuarioRepository = UsuarioRepository;
+    }
+    
+    public Usuario validateLogin(String username, String password){
+        Usuario usuario = UsuarioRepository.findByUsername(username);
+        
+        String hashDigitado = Criptografia.getMD5(password);
+        
+        if(!hashDigitado.equals(usuario.getPassword_hash())){
+            throw new InvalidLoginException("Senha incorreta.");
+        }
+        
+        return usuario;
+    }
 }
